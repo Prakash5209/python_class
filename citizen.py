ab57b11150032b04067a9d4481fbac3254fc938c
Prakash Kumar Chaudhary <prakashkchaudhary5209@gmail.com>
2023-04-21 08:34:46 +0545

    i am batman

--- citizen.py
+++ citizen.py
@@ -4,5 +4,7 @@
 # print(list(filter(lambda x:x.startswith('w'),lst)))
 
 
-lst = [i for i in range(10)]
-print(list(map(lambda x:x*2,filter(lambda x:x %2 == 0,lst))))
+def function(**kwargs):
+    print(kwargs)
+    
+function('nice,asd')
